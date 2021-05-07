52b176838d4c859b7b26e6fe4de5a905eca53074
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-05-07 18:17:38 +0100

    Added npm watch dependency

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -56,28 +56,39 @@ const data = [
 
 export default function Metrics() {
   return (
-    <LineChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}>
-      <CartesianGrid strokeDasharray='3 3' />
-      <XAxis dataKey='name' />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line
-        type='monotone'
-        dataKey='pv'
-        stroke='#8884d8'
-        activeDot={{ r: 8 }}
-      />
-      <LineChart type='monotone' dataKey='uv' stroke='#82ca9d' />
-    </LineChart>
+    <>
+      <div className='row'>
+        <div className='col'>
+          <div className='card'>
+            <div className='card-body'>
+              <h5>Metrics</h5>
+              <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}>
+                <CartesianGrid strokeDasharray='3 3' />
+                <XAxis dataKey='name' />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type='monotone'
+                  dataKey='pv'
+                  stroke='#8884d8'
+                  activeDot={{ r: 8 }}
+                />
+                <LineChart type='monotone' dataKey='uv' stroke='#82ca9d' />
+              </LineChart>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
